98f81184ed2eaf5882c5b5afcf3830054d769d51
MarinaGalanina <marina.galanina.2000@mail.ru>
2023-08-30 21:15:34 +0200

    Change in backend for user file manipulation

--- backend.py
+++ backend.py
@@ -7,79 +7,85 @@ from nltk.stem import WordNetLemmatizer
 import tensorflow as tf
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, Dropout
-from parsing_json import data
+#from parsing_json import data
 import tkinter as tk
 nltk.download("punkt")
 nltk.download("wordnet")
 
-words = [] #For Bow model/ vocabulary for patterns
-classes = [] #For Bow  model/ vocabulary for tags
+#words = [] #For Bow model/ vocabulary for patterns
+# classes = [] #For Bow  model/ vocabulary for tags
 data_X = [] #For storing each pattern
 data_y = [] #For storing tag corresponding to each pattern in data_X
-# Iterating over all the intents
-
-for intent in data["intents"]:
-    for pattern in intent["patterns"]:
-        tokens = nltk.word_tokenize(pattern) # tokenize each pattern
-        words.extend(tokens) #and append tokens to words
-        data_X.append(pattern) #appending pattern to data_X
-        data_y.append(intent["tag"]) ,# appending the associated tag to each pattern
-
-    # adding the tag to the classes if it's not there already
-    if intent["tag"] not in classes:
-        classes.append(intent["tag"])
-
-# initializing lemmatizer to get stem of words
 lemmatizer = WordNetLemmatizer()
+# Iterating over all the intents
+def train_model(data):
+    words = []  # For Bow model/ vocabulary for patterns
+    classes = []  # For Bow  model/ vocabulary for tags
 
-# lemmatize all the words in the vocab and convert them to lowercase
-# if the words don't appear in punctuation
-words = [lemmatizer.lemmatize(word.lower()) for word in words if word not in string.punctuation]
-# sorting the vocab and classes in alphabetical order and taking the # set to ensure no duplicates occur
-words = sorted(set(words))
-classes = sorted(set(classes))
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            tokens = nltk.word_tokenize(pattern) # tokenize each pattern
+            words.extend(tokens) #and append tokens to words
+            data_X.append(pattern) #appending pattern to data_X
+            data_y.append(intent["tag"]) ,# appending the associated tag to each pattern
 
-training = []
-out_empty = [0] * len(classes)
-# creating the bag of words model
-for idx, doc in enumerate(data_X):
-    bow = []
-    text = lemmatizer.lemmatize(doc.lower())
-    for word in words:
-        bow.append(1) if word in text else bow.append(0)
-    # mark the index of class that the current pattern is associated
-    # to
-    output_row = list(out_empty)
-    output_row[classes.index(data_y[idx])] = 1
-    # add the one hot encoded BoW and associated classes to training
-    training.append([bow, output_row])
-# shuffle the data and convert it to an array
-random.shuffle(training)
-training = np.array(training, dtype=object)
-# split the features and target labels
-train_X = np.array(list(training[:, 0]))
-train_Y = np.array(list(training[:, 1]))
+        # adding the tag to the classes if it's not there already
+        if intent["tag"] not in classes:
+            classes.append(intent["tag"])
 
-model = Sequential()
-model.add(Dense(128, input_shape=(len(train_X[0]),), activation="relu"))
-model.add(Dropout(0.5))
-model.add(Dense(64, activation="relu"))
-model.add(Dropout(0.5))
-model.add(Dense(len(train_Y[0]), activation = "softmax"))
-adam = tf.keras.optimizers.legacy.Adam(learning_rate=0.01, decay=1e-6)
-model.compile(loss='categorical_crossentropy',
-              optimizer=adam,
-              metrics=["accuracy"])
-print(model.summary())
-model.fit(x=train_X, y=train_Y, epochs=250, verbose=1)
+    # initializing lemmatizer to get stem of words
+    # lemmatizer = WordNetLemmatizer()
 
-def clean_text(text):
+    # lemmatize all the words in the vocab and convert them to lowercase
+    # if the words don't appear in punctuation
+    words = [lemmatizer.lemmatize(word.lower()) for word in words if word not in string.punctuation]
+    # sorting the vocab and classes in alphabetical order and taking the # set to ensure no duplicates occur
+    words = sorted(set(words))
+    classes = sorted(set(classes))
+
+    training = []
+    out_empty = [0] * len(classes)
+    # creating the bag of words model
+    for idx, doc in enumerate(data_X):
+        bow = []
+        text = lemmatizer.lemmatize(doc.lower())
+        for word in words:
+            bow.append(1) if word in text else bow.append(0)
+        # mark the index of class that the current pattern is associated
+        # to
+        output_row = list(out_empty)
+        output_row[classes.index(data_y[idx])] = 1
+        # add the one hot encoded BoW and associated classes to training
+        training.append([bow, output_row])
+    # shuffle the data and convert it to an array
+    random.shuffle(training)
+    training = np.array(training, dtype=object)
+    # split the features and target labels
+    train_X = np.array(list(training[:, 0]))
+    train_Y = np.array(list(training[:, 1]))
+
+    model = Sequential()
+    model.add(Dense(128, input_shape=(len(train_X[0]),), activation="relu"))
+    model.add(Dropout(0.5))
+    model.add(Dense(64, activation="relu"))
+    model.add(Dropout(0.5))
+    model.add(Dense(len(train_Y[0]), activation = "softmax"))
+    adam = tf.keras.optimizers.legacy.Adam(learning_rate=0.01, decay=1e-6)
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=adam,
+                  metrics=["accuracy"])
+    print(model.summary())
+    model.fit(x=train_X, y=train_Y, epochs=250, verbose=1)
+    print("Model is ready")
+    return model, words, classes
+
+def clean_text(text, word):
   tokens = nltk.word_tokenize(text)
   tokens = [lemmatizer.lemmatize(word) for word in tokens]
   return tokens
 
 def bag_of_words(text, vocab):
-  tokens = clean_text(text)
+  tokens = clean_text(text, vocab)
   bow = [0] * len(vocab)
   for w in tokens:
     for idx, word in enumerate(vocab):
@@ -87,7 +93,7 @@ def bag_of_words(text, vocab):
         bow[idx] = 1
   return np.array(bow)
 
-def pred_class(text, vocab, labels):
+def pred_class(text, model, vocab, labels):
   bow = bag_of_words(text, vocab)
   result = model.predict(np.array([bow]))[0] #Extracting probabilities
   thresh = 0.5
@@ -110,8 +116,8 @@ def get_response(intents_list, intents_json):
         break
   return result
 
-def chatbot_response(msg):
-    ints = pred_class(msg, words, classes)
+def chatbot_response(msg, model, data, words, classes):
+    ints = pred_class(msg, model, words, classes)
     res = get_response(ints, data)
     return res
 print("Press 0 if you don't want to chat with our ChatBot.")
