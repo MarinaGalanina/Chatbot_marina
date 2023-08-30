ce86f938c480315444a4202faee2225530d717a3
MarinaGalanina <marina.galanina.2000@mail.ru>
2023-08-30 21:18:06 +0200

    Changes in parsing and app to upload files manually

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template, request, jsonify
-from backend import chatbot_response
+import backend
+from backend import chatbot_response, train_model
+from parsing_json import crete_dataset
+import os
 
 app = Flask(__name__)
+language_model = None
+
+words_ = None
+data_ = None
+classes_ = None
 
 @app.route('/')
 def home():
@@ -10,7 +18,30 @@ def home():
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
-    return chatbot_response(userText)
+    if language_model is None:
+        return "Sorry! I don't understand."
+    return chatbot_response(userText, language_model, data_, words_, classes_)
+
+@app.route("/upload_and_process", methods=["POST"])
+def upload_and_process():
+    uploaded_file = request.files.get("file")
+
+    if uploaded_file:
+        file_path = os.path.join(os.getcwd(), "uploads", uploaded_file.filename)
+        uploaded_file.save(file_path)
+
+        # Process the uploaded file (e.g., read contents)
+        with open(file_path, "r") as file:
+            file_contents = file.read()
+            global data_
+            intents, pairs, data_ = crete_dataset(file_path)
+            global words_, classes_, language_model
+            language_model, words_, classes_ = train_model(data_)
+    # return 0
+        return f"Uploaded and processed file with {len(file_contents)} characters."
+    else:
+        return "No file uploaded."
+
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
--- parsing_json.py
+++ parsing_json.py
@@ -1,32 +1,38 @@
 import os
 import json
 
-data_root='C:/Users/admin/Downloads/chatbot'
+def crete_dataset(file_path: str):
+    #data_root='C:/Users/admin/Downloads/chatbot'
 
-with open('C:/Users/admin/Downloads/chatbot/training.txt', 'r') as file:
-    input_text = file.read()
-pairs = input_text.strip().split('\n\n')
+    with open(file_path, 'r') as file:
+        input_text = file.read()
 
-intents = []
-for pair in pairs:
-    question, answer = pair.split('\n', 1)
-    intent = {
-        "tag": question.strip(' ?'),
-        "patterns": [question.strip(' ?')],
-        "responses": [answer.strip()]
+
+    pairs = input_text.strip().split('\n\n')
+
+    intents = []
+    for pair in pairs:
+        question, answer = pair.split('\n', 1)
+        intent = {
+            "tag": question.strip(' ?'),
+            "patterns": [question.strip(' ?')],
+            "responses": [answer.strip()]
+        }
+        intents.append(intent)
+
+
+    json_data = {
+        "intents": intents
     }
-    intents.append(intent)
+    data = json_data
 
+    # json_file_path = os.path.join(data_root, 'intents.json')
+    # with open(json_file_path, 'w') as json_file:
+    #     json.dump(json_data, json_file, indent=4)
+    # print(f"JSON data saved to: {json_file_path}")
+    #
+    #
+    # data_file = open(data_root + '/intents.json').read()
+    # data = json.loads(data_file)
 
-json_data = {
-    "intents": intents
-}
-
-json_file_path = os.path.join(data_root, 'intents.json')
-with open(json_file_path, 'w') as json_file:
-    json.dump(json_data, json_file, indent=4)
-print(f"JSON data saved to: {json_file_path}")
-
-
-data_file = open(data_root + '/intents.json').read()
-data = json.loads(data_file)
+    return intents, pairs, data
